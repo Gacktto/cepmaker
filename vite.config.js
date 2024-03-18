9ec0a910f7c6d0460f2a52bbccb5d118a30f0de5
Gacktto <mandrafaquer123@gmail.com>
2024-03-18 20:46:20 -0300

    updating vite

--- vite.config.js
+++ vite.config.js
@@ -14,5 +14,13 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  server: {
+    // Configurando o fallback para index.html
+    // Isso é necessário para permitir que o Vue Router gerencie as rotas em modo hash
+    // em vez de enviar um 404 quando a URL é atualizada manualmente.
+    fs: {
+      strict: false
+    }
+  },
   assetsInclude: ['**/*.csv']
 })
